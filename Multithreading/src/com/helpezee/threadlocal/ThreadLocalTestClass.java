316a0adac689f1e8b6b38b48affe7604bf5f3e4d
satya <satya.iton@gmail.com>
2019-04-15 19:28:12 +0530

    ThreadLocal Example

---
+++ Multithreading/src/com/helpezee/threadlocal/ThreadLocalTestClass.java
@@ -0,0 +1,39 @@
+package com.helpezee.threadlocal;
+
+class MyThreadLocalDemo extends Thread {
+
+	ThreadLocal<Integer> threadLocalValue = new ThreadLocal<>();
+	int value;
+
+	public MyThreadLocalDemo(String name) {
+		super(name);
+	}
+
+	public void run() {
+		threadLocalValue.set((int) (Math.random() * 1000));
+		value = threadLocalValue.get();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Thread Local Value for " + Thread.currentThread().getName() + " : " + threadLocalValue.get());
+		System.out.println("Thread value for "+Thread.currentThread().getName()+" value "+value);
+	}
+}
+
+public class ThreadLocalTestClass {
+	public static void main(String args[]){
+		
+		MyThreadLocalDemo t1 = new MyThreadLocalDemo("First");
+		MyThreadLocalDemo t2 = new MyThreadLocalDemo("Second");
+		MyThreadLocalDemo t3 = new MyThreadLocalDemo("Third");
+		
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+}
+
+//Refer : https://helpezee.wordpress.com/2019/04/15/threadlocal/
