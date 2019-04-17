a5f83f2a7199303aee7efe29427f1151e4642aaf
satya <satya.iton@gmail.com>
2019-04-17 20:26:36 +0530

    Fork and Join pool

--- Multithreading/src/com/helpezee/deadlock/TestDeadLockExample.java
+++ Multithreading/src/com/helpezee/deadlock/TestDeadLockExample.java
@@ -2,19 +2,19 @@ package com.helpezee.deadlock;
 
 public class TestDeadLockExample {
 
-	public static Object lock1=new Object();
-	public static Object lock2=new Object();
-	
+	public static Object lock1 = new Object();
+	public static Object lock2 = new Object();
+
 	public static void main(String[] args) {
-		MyThread1 t1=new MyThread1();
-		MyThread2 t2=new MyThread2();
+		MyThread1 t1 = new MyThread1();
+		MyThread2 t2 = new MyThread2();
 		t1.start();
 		t2.start();
 
 	}
-	
-	public static class MyThread1 extends Thread{
-		public void run(){
+
+	public static class MyThread1 extends Thread {
+		public void run() {
 			synchronized (lock1) {
 				System.out.println("Thread 1 Holding Lock 1");
 				try {
@@ -23,15 +23,15 @@ public class TestDeadLockExample {
 					e.printStackTrace();
 				}
 				synchronized (lock2) {
-					System.out.println("Thread 1 Holding Lock1 and Lock2");
+					System.out.println("Thread 1 Holding Lock 1 and Lock2");
 				}
 			}
 		}
 	}
 
-	public static class MyThread2 extends Thread{
-		public void run(){
-			synchronized (lock2) {
+	public static class MyThread2 extends Thread {
+		public void run() {
+			synchronized (lock2) {//If we change the order of locks in synchronized block, Deadlock will resolve
 				System.out.println("Thread 2 Holding Lock 2");
 				try {
 					Thread.sleep(1000);
@@ -39,7 +39,7 @@ public class TestDeadLockExample {
 					e.printStackTrace();
 				}
 				synchronized (lock1) {
-					System.out.println("Thread 2 Holding Lock1 and Lock2");
+					System.out.println("Thread 2 Holding Lock 1 and Lock2");
 				}
 			}
 		}
